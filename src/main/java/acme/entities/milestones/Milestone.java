b2f3dec77de90fc5708b910cfe177535a8ecfe75
MML9796 <sansilama@alum.us.es>
2026-02-16 10:47:00 +0100

    Creados los modelC2 (#8)

---
+++ src/main/java/acme/entities/milestones/Milestone.java
@@ -0,0 +1,51 @@
+
+package acme.entities.milestones;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import lombok.Getter;
+import lombok.Setter;
+import validation.ValidHeader;
+import validation.ValidText;
+
+@Entity
+@Getter
+@Setter
+public class Milestone extends AbstractEntity {
+
+	// Serialisation identifier
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				title;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				achievements;
+
+	@Mandatory
+	@ValidNumber(min = 0.0)
+	@Column
+	private Double				effort;
+
+	@Mandatory
+	@Valid
+	@Column
+	private MilestoneKind		kind;
+
+	//Relationship
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Campaign			campaign;
+}
